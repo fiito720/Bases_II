053b723825cc9d77c38cce72c5732afddef2f3c7
David Acuna <rodolfoide69@gmail.com>
2023-02-15 17:43:00 -0600

    New Inserts

--- Caso1/Case1_Inserts.sql
+++ Caso1/Case1_Inserts.sql
@@ -1,110 +1,59 @@
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(1, 1, 'Manzana');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(2, 1, 'Uva');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(3, 1, 'Zanahoria');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(4, 1, 'Masa');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(5, 2, 'Tabasco');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(6, 2, 'Salsa de tomate');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(7, 1, 'Maiz');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(8, 2, 'Mostaza');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(9, 1, 'Frijoles');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(10, 2, 'Leche');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(11, 1, 'Queso');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(12, 1, 'Salchichon');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(13, 1, 'Culantro');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(14, 1, 'Tomillo');
 
-INSERT INTO Product(idProduct, idUnit, nameProduct)
+INSERT INTO Products(idProduct, idUnit, nameProduct)
 VALUES(15, 2, 'Coca cola');
 
-INSERT INTO Unit(idUnit, nameUnit)
+INSERT INTO Units(idUnit, nameUnit)
 VALUES(1, 'Gramo');
 
-INSERT INTO Unit(idUnit, nameUnit)
+INSERT INTO Units(idUnit, nameUnit)
 VALUES(2, 'Mililitro');
 
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(1, 1,'David', 'xxx');
+INSERT INTO Genders(idGender, descriptionGender)
+VALUES(1, 'Femanino');
 
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(2, 1,'Rodolfo', 'xxx');
+INSERT INTO Genders(idGender, descriptionGender)
+VALUES(2, 'Masculino');
 
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(3, 1,'Ana', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(4, 1,'Maria', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(5, 1,'Carlos', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(6, 1,'Diego', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(7, 1,'Luis', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(8, 1,'Marcos', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(9, 1,'Laura', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(10, 1,'Jimena', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(11, 1,'Mauren', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(12, 1,'Ian', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(13, 1,'Monserrat', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(14, 1,'Fiorella', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(15, 1,'Ruddy', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(16, 1,'Tannia', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(17, 1,'Michelle', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(18, 1,'Suriany', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(19, 1,'Fabiola', 'xxx');
-
-INSERT INTO Users(idUser, idUserType, userName, userPassword)
-VALUES(20, 1,'Angie', 'xxx');
+INSERT INTO Person(idPerson, idGender, idAddress, birthday, namePerson, userName, password, creationDate)
+VALUES(1, 2, 1, '1999/11/15', 'David', 'xxx', 'xxx', '2023/02/15');
